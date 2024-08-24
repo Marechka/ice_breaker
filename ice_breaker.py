942b2f532f79312705297c7d128c9a0f172f030c
Masha <39933741+Marechka@users.noreply.github.com>
2024-08-24 16:01:04 -0700

    OpenAI call set up

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,2 +1,23 @@
-if __name__ == '__main__':
-    print('Hello')
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+information = """
+"""
+if __name__ == "__main__":
+    print("Hello")
+
+    summary_template = """
+        given the information {information} about a person from what I want you to create:
+        1. a short summary
+        2. two interesting facts about them
+        """
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = summary_prompt_template | llm
+    res = chain.invoke(input={"information": information})
+
+    print(res)
