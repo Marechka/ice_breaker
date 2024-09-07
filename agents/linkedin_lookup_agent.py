f51fe011811ce7f12b0ce1e1116283323aaa886f
Masha <39933741+Marechka@users.noreply.github.com>
2024-09-06 17:05:33 -0700

    set up agent and tool to find a linkedin url

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -1,5 +1,10 @@
 import os
+
+from click import prompt
 from dotenv import load_dotenv
+from langchain.chains.summarize.refine_prompts import prompt_template
+
+from tools.tools import get_profile_url_tavily
 
 load_dotenv()
 
@@ -8,6 +13,35 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain_core.tools import Tool
 from langchain.agents import (create_react_agent, AgentExecutor)
 
+from langchain import hub
+
 
 def lookup(name: str) -> str:
-    return "https://linkedin.com/in/johnrmarty/"
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    template = """given the full name {name_of_person} I want you to get it me a link to their Linkedin profile page.
+                                  Your answer should contain only a URL"""
+    prompt_template = PromptTemplate(template=template, input_variables=["name_of_person"])
+
+    tools_for_agent = [
+        Tool(
+            name="Crawl Google 4 linkedin profile page",
+            func=get_profile_url_tavily, #pattern function
+            description="useful for when you need to get Linkedin Page URL"
+        )
+    ]
+
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools = tools_for_agent,prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+
+    result = agent_executor.invoke(
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    linked_profile_url = result["output"]
+    return linked_profile_url
+    # return "https://linkedin.com/in/johnrmarty/"
+
+if __name__ == "__main__":
+    print(lookup(name="Masha Volska"))
